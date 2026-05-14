578cc1a1b5770ff7b6fef0e3c059f56155d99fde
AITUS95 <rosarioaita577@gmail.com>
2026-05-14 10:19:18 +0200

    Revert "Defer Xbox TB lookup logging check"
    This reverts commit d921b2deea1f180b778cf2774e5e6e44d3979b3a.

--- accel/tcg/cpu-exec.c
+++ accel/tcg/cpu-exec.c
@@ -626,15 +626,16 @@ const void *HELPER(lookup_tb_ptr)(CPUArchState *env)
     uint64_t cs_base;
     uint64_t state_tag;
     uint32_t hash;
+    bool log_tb;
 
     xbox_get_tb_cpu_state_fast(env, &pc, &flags, &cs_base);
     cflags = curr_cflags(cpu);
     state_tag = tb_jmp_cache_state_tag_parts(flags, cflags);
+    log_tb = unlikely(qemu_loglevel_mask(CPU_LOG_TB_CPU | CPU_LOG_EXEC));
 
     if (unlikely(!QTAILQ_EMPTY(&cpu->breakpoints))) {
         return xbox_lookup_tb_ptr_breakpoints(cpu, pc, cs_base, state_tag,
-                                             qemu_loglevel_mask(CPU_LOG_TB_CPU |
-                                                                CPU_LOG_EXEC));
+                                             log_tb);
     }
 
     hash = tb_jmp_cache_hash_func(pc);
@@ -647,7 +648,7 @@ const void *HELPER(lookup_tb_ptr)(CPUArchState *env)
         }
     }
 
-    if (unlikely(qemu_loglevel_mask(CPU_LOG_TB_CPU | CPU_LOG_EXEC))) {
+    if (log_tb) {
         return xbox_lookup_tb_ptr_log(pc, cpu, tb);
     }
 
